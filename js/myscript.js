1627596e065ca9cc2ca5edd12933f31176d192f3
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-10 17:36:32 +0200

    aggiunte note "todo"

--- js/myscript.js
+++ js/myscript.js
@@ -3,12 +3,17 @@ $(document).ready(function() {
   $('.dropdown').mouseenter(function() {
     $('.dropdown .menu-dp').addClass('hide_dp');
     $(this).children('.dropdown .menu-dp').removeClass('hide_dp');
+
+
+
   });
 
 // al click del mouse la funzione rimuove(o aggiunge) la classe hide
   $('.dropdown').click(function() {
     $(this).children('.menu-dp').toggleClass('hide_dp');
-
    });
 
 })
+
+// TODO: aggiungere after nuovo ad alcuni elementi dell li 
+// TODO: far cambiare il verso dell icona after IT
