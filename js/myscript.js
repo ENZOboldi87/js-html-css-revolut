d16db1ab2be0a06ef699f54aba81efde48be4341
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-10 16:03:07 +0200

    working on js

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $('.dropdown').mouseenter(function() {
+    $(this).children('.dropdown .menu-dp').toggleClass('fornow');
+  });
+
+  // $('.dropdown').mouseleave(function() {
+  //   $(this).children('.dropdown .menu-dp').toggleClass('fornow');
+  // });
+
+  $('.dropdown').click(function() {
+    $(this).children('.menu-dp').toggleClass('fornow');
+  });
+
 });
