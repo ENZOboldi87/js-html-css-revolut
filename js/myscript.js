dceb934b7e4de845b4a6c885f5ea9883a6e3eda5
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-10 17:04:25 +0200

    mancano i particolari

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
+  // al passaggio del mouse rimuove la classe hide,
   $('.dropdown').mouseenter(function() {
-    $(this).children('.dropdown .menu-dp').toggleClass('fornow');
+    $('.dropdown .menu-dp').addClass('hide_dp');
+    $(this).children('.dropdown .menu-dp').removeClass('hide_dp');
   });
 
-  // $('.dropdown').mouseleave(function() {
-  //   $(this).children('.dropdown .menu-dp').toggleClass('fornow');
-  // });
-
+// al click del mouse la funzione rimuove(o aggiunge) la classe hide
   $('.dropdown').click(function() {
-    $(this).children('.menu-dp').toggleClass('fornow');
-  });
+    $(this).children('.menu-dp').toggleClass('hide_dp');
 
-});
+   });
+
+})
